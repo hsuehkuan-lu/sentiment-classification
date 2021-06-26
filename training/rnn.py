a0a6324c2c659f689ea5f02bb60c607f32d9f07e
hsuehkuan-lu <s810142000@gmail.com>
2021-06-26 22:40:56 +0800

    init lstm with double layers

--- training/rnn.py
+++ training/rnn.py
@@ -15,6 +15,10 @@ with open('params.yaml', 'r') as f:
 class LSTMTrainer(TrainerBase):
     def __init__(self, model, train_dataloader, valid_dataloader=None):
         super(LSTMTrainer, self).__init__(model, train_dataloader, valid_dataloader)
+        self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=PARAMS['train']['optimizer']['lr'])
+        self._scheduler = torch.optim.lr_scheduler.StepLR(
+            self._optimizer, PARAMS['train']['optimizer']['step_lr'], gamma=PARAMS['train']['optimizer']['gamma']
+        )
 
     @property
     def method(self):
