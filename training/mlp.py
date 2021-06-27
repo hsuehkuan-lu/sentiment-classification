3061fcd5dab8a204af047bcac227db42b2a5429b
hsuehkuan-lu <s810142000@gmail.com>
2021-06-27 23:57:31 +0800

    update mlp model with l2 regularization

--- training/mlp.py
+++ training/mlp.py
@@ -9,7 +9,8 @@ with open('params.yaml', 'r') as f:
 class Trainer(TrainerBase):
     def __init__(self, model):
         super(Trainer, self).__init__(model)
-        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=PARAMS['train']['optimizer']['lr'])
+        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=PARAMS['train']['optimizer']['lr'],
+                                           weight_decay=PARAMS['train']['optimizer']['weight_decay'])
         self._scheduler = torch.optim.lr_scheduler.StepLR(
             self._optimizer, PARAMS['train']['optimizer']['step_lr'], gamma=PARAMS['train']['optimizer']['gamma']
         )
