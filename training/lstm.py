0cc6ecee0c3b44d2bce2dfbfb6e7ccf98da1aff1
hsuehkuan-lu <s810142000@gmail.com>
2021-06-27 22:05:46 +0800

    fix lstm

--- training/lstm.py
+++ training/lstm.py
@@ -7,8 +7,8 @@ with open('params.yaml', 'r') as f:
 
 
 class Trainer(TrainerBase):
-    def __init__(self, model, train_dataloader, valid_dataloader=None):
-        super(Trainer, self).__init__(model, train_dataloader, valid_dataloader)
+    def __init__(self, model):
+        super(Trainer, self).__init__(model)
         self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=PARAMS['train']['optimizer']['lr'])
         self._scheduler = torch.optim.lr_scheduler.StepLR(
             self._optimizer, PARAMS['train']['optimizer']['step_lr'], gamma=PARAMS['train']['optimizer']['gamma']
