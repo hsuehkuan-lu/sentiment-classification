538c49e8df6fa7bbeeeac0be17fdbe3a3b42276c
hsuehkuan-lu <s810142000@gmail.com>
2021-06-28 00:39:46 +0800

    add lstm weight decay

--- training/lstm.py
+++ training/lstm.py
@@ -10,7 +10,7 @@ class Trainer(TrainerBase):
     def __init__(self, model):
         super(Trainer, self).__init__(model)
         self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=PARAMS['train']['optimizer']['lr'],
-                                              weight_decay=PARAMS['train']['optimizer']['weight_decay'])
+                                              weight_decay=float(PARAMS['train']['optimizer']['weight_decay']))
         self._scheduler = torch.optim.lr_scheduler.StepLR(
             self._optimizer, PARAMS['train']['optimizer']['step_lr'], gamma=PARAMS['train']['optimizer']['gamma']
         )
