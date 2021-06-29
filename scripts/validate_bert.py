bfce3c9d0a2332f0f67c961f800a7ae70e4dabdf
hsuehkuan-lu <s810142000@gmail.com>
2021-06-29 23:40:57 +0800

    replace k-fold cross validation with train_test split (reduce computations)

--- scripts/validate_bert.py
+++ scripts/validate_bert.py
@@ -7,7 +7,7 @@ import importlib
 import numpy as np
 import pandas as pd
 from pathlib import Path
-from sklearn.model_selection import KFold
+from sklearn.model_selection import train_test_split
 from data_loader.bert_dataloaders import DataFrameDataLoader
 from dotenv import load_dotenv
 
@@ -19,64 +19,52 @@ with open('params.yaml', 'r') as f:
 
 
 def start_validating(method='basic'):
-    kf = KFold(n_splits=PARAMS['validate']['kfold'], shuffle=True, random_state=PARAMS['seed'])
     df = pd.read_csv('data/train.csv')
+    train_df, valid_df = train_test_split(df, test_size=1. / PARAMS['validate']['kfold'], random_state=PARAMS['seed'])
     total_results = list()
     total_losses = list()
-    for idx, (train_index, valid_index) in enumerate(kf.split(df)):
-        print(f"Cross validation {idx}-fold")
-        try:
-            model_module = importlib.import_module(f'model.bert.{method}')
-            model = model_module.Model(**PARAMS['bert'][method])
-        except Exception as e:
-            raise e
-        if torch.cuda.is_available():
-            device = torch.device('cuda', PARAMS.get('gpu', 0))
-        else:
-            device = torch.device('cpu')
-        model.to(device)
+    print(f"Train valid split")
+    try:
+        model_module = importlib.import_module(f'model.bert.{method}')
+        model = model_module.Model(**PARAMS['bert'][method])
+    except Exception as e:
+        raise e
+    if torch.cuda.is_available():
+        device = torch.device('cuda', PARAMS.get('gpu', 0))
+    else:
+        device = torch.device('cpu')
+    model.to(device)
 
-        try:
-            trainer_module = importlib.import_module(f'training.bert.{method}')
-            trainer = trainer_module.Trainer(model, mode='validate')
-        except Exception as e:
-            raise e
+    try:
+        trainer_module = importlib.import_module(f'training.bert.{method}')
+        trainer = trainer_module.Trainer(model, mode='validate')
+    except Exception as e:
+        raise e
 
-        train_df = df.iloc[train_index]
-        valid_df = df.iloc[valid_index]
+    train_dataloader = DataFrameDataLoader(
+        train_df, batch_size=PARAMS['validate']['batch_size'],
+        shuffle=PARAMS['validate']['shuffle'], max_len=PARAMS['bert']['max_len']
+    )
+    valid_dataloader = DataFrameDataLoader(
+        valid_df, batch_size=PARAMS['validate']['batch_size'],
+        shuffle=PARAMS['validate']['shuffle'], max_len=PARAMS['bert']['max_len']
+    )
 
-        train_dataloader = DataFrameDataLoader(
-            train_df, batch_size=PARAMS['validate']['batch_size'],
-            shuffle=PARAMS['validate']['shuffle'], max_len=PARAMS['bert']['max_len']
-        )
-        valid_dataloader = DataFrameDataLoader(
-            valid_df, batch_size=PARAMS['validate']['batch_size'],
-            shuffle=PARAMS['validate']['shuffle'], max_len=PARAMS['bert']['max_len']
-        )
+    trainer.set_dataloader(train_dataloader, valid_dataloader)
 
-        trainer.set_dataloader(train_dataloader, valid_dataloader)
+    results, losses = trainer.validate()
 
-        results, losses = trainer.validate()
-        total_results.append(results)
-        for loss in losses:
-            loss['fold'] = idx + 1
-        total_losses += losses
-    print(total_losses)
-    columns = list(total_losses[0].keys())
-    total_losses_df = pd.DataFrame(total_losses, columns=columns)
+    columns = list(losses[0].keys())
+    losses_df = pd.DataFrame(losses, columns=columns)
 
-    average_results = dict()
-    for score in ('accuracy', 'precision', 'recall', 'f1-score'):
-        average_results[score] = np.mean([results[score] for results in total_results])
-    print(average_results)
-    return average_results, total_losses_df
+    return results, losses_df
 
 
 if __name__ == '__main__':
     method = sys.argv[1]
-    average_results, total_losses_df = start_validating(method)
+    results, losses_df = start_validating(method)
     results_path = Path(os.getenv('OUTPUT_PATH'), f'bert-{method}_validate_{os.getenv("RESULTS_PATH")}')
     with open(results_path, 'w') as f:
-        json.dump(average_results, f)
+        json.dump(results, f)
     plots_path = Path(os.getenv('OUTPUT_PATH'), f'bert-{method}_validate_{os.getenv("PLOTS_PATH")}')
-    total_losses_df.to_csv(plots_path, index=False)
+    losses_df.to_csv(plots_path, index=False)
