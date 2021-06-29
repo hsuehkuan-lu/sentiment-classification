a1fbbbae81a13a4c9004253668f89d103bdc0368
Hsuehkuan Lu <hsuehkuan.peter.lu@gmail.com>
2021-06-29 08:36:31 +0800

    Update mlp.py

--- training/mlp.py
+++ training/mlp.py
@@ -9,7 +9,7 @@ with open('params.yaml', 'r') as f:
 class Trainer(TrainerBase):
     def __init__(self, model, mode):
         super(Trainer, self).__init__(model, mode)
-        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=PARAMS[mode]['optimizer']['lr'],
+        self._optimizer = torch.optim.Adam(self._model.parameters(), lr=float(PARAMS[mode]['optimizer']['lr']),
                                            weight_decay=float(PARAMS[mode]['optimizer']['weight_decay']))
         self._scheduler = torch.optim.lr_scheduler.StepLR(
             self._optimizer, PARAMS[mode]['optimizer']['step_lr'], gamma=PARAMS[mode]['optimizer']['gamma']
