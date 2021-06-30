b7aa5365ceb8c8672443dbf503d6aa4749fb70ae
Peter Lu <Peter_HK_Lu@wistron.com>
2021-06-30 09:04:01 +0800

    add mlp after bert

--- model/bert/basic.py
+++ model/bert/basic.py
@@ -1,22 +1,38 @@
 import torch
 import torch.nn as nn
+from collections import OrderedDict
 from model.base import ModelBase
 from transformers import BertModel
 
 
 class Model(ModelBase):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, n_layers, hidden_size, dropout, *args, **kwargs):
         super(Model, self).__init__()
         # [B x L] -> [B x L x D], [B x D]
         self.bert = BertModel.from_pretrained('bert-base-uncased')
-        self.out = nn.Linear(768, 1)
+        layers = [
+            ('fc1', nn.Linear(768, hidden_size)),
+            ('drop1', nn.Dropout(dropout)),
+            ('relu1', nn.Relu())
+        ]
+        for i in range(1, n_layers+1):
+            layers += [
+                (f'fc{i}', nn.Linear(768))
+                ('drop{i}', nn.Dropout(dropout)),
+                ('relu{i}', nn.Relu())
+            ]
+        layers += [
+            ('out', nn.Linear(hidden_size, 1)),
+            ('sigmoid', nn.Sigmoid())
+        ]
+        self.out = nn.Sequential(layers)
         self.init_weights()
 
     def forward(self, tokens, masks=None):
         # BERT
         # [B x L x D], [B, D] (pooled_outputs)
         outputs = self.bert(tokens, attention_mask=masks)
-        return self.out(outputs.pooler_output).sigmoid()
+        return self.out(outputs.pooler_output)
 
     def load_model(self, model_path):
         self.load_state_dict(torch.load(model_path))
@@ -25,8 +41,10 @@ class Model(ModelBase):
     def init_weights(self):
         for p in self.bert.parameters():
             p.requires_grad = False
-        nn.init.xavier_normal_(self.out.weight)
-        nn.init.constant_(self.out.bias, 0)
+        for m in self.conv.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight)
+                nn.init.constant_(m.bias, 0)
 
     def save_model(self, model_path):
         torch.save(self.state_dict(), model_path)
