61c7af57ca15fb217f829db34fd9390fc6378233
Peter Lu <Peter_HK_Lu@wistron.com>
2021-07-05 13:11:07 +0800

    update loss

--- training/base.py
+++ training/base.py
@@ -47,7 +47,7 @@ class TrainerBase(abc.ABC):
         eval_preds, eval_labels = list(), list()
         all_preds, all_labels = list(), list()
         log_interval = PARAMS['log_interval']
-        total_loss = list()
+        total_loss, eval_loss = list(), list()
         start_time = time.time()
 
         for idx, (label, text, offsets) in enumerate(tqdm(dataloader)):
@@ -70,13 +70,15 @@ class TrainerBase(abc.ABC):
                         '| elapsed {} | {:5d}/{:5d} batches | loss {:8.3f} | '
                         'valid accuracy {:8.3f} | precision {:8.3f} | '
                         'recall {:8.3f} | f1-score {:8.3f}'.format(
-                            elapsed, idx, len(dataloader), loss.item(), (eval_labels == eval_preds).mean(),
+                            elapsed, idx, len(dataloader), np.mean(eval_loss), (eval_labels == eval_preds).mean(),
                             prf[0], prf[1], prf[2]
                         )
                     )
                     eval_preds, eval_labels = list(), list()
+                    eval_loss = list()
                     start_time = time.time()
-            total_loss += [loss.item()]
+            total_loss += [float(loss)]
+            eval_loss += [float(loss)]
             preds = (predicted_label > 0.5).squeeze(dim=-1)
             p = preds.detach().cpu().numpy()
             l = label.detach().cpu().numpy()
