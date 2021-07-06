d11f27e08e74f79a7c4e1d1349886e83bf71a131
hsuehkuan-lu <s810142000@gmail.com>
2021-07-06 22:43:12 +0800

    update bert training and inference

--- scripts/inference_bert.py
+++ scripts/inference_bert.py
@@ -16,17 +16,20 @@ with open('params.yaml', 'r') as f:
     PARAMS = yaml.safe_load(f)
 
 
-def inference(bert_model, method='lstm'):
+def inference(bert_model, pretrained_model, method='lstm'):
     try:
         model_module = importlib.import_module(f'model.{bert_model}.{method}')
-        model = model_module.Model(**PARAMS[bert_model], **PARAMS[bert_model][method])
+        model = model_module.Model(
+            **PARAMS[bert_model], **PARAMS[bert_model][method],
+            pretrained_model=pretrained_model
+        )
     except Exception as e:
         raise e
     if torch.cuda.is_available():
         device = torch.device('cuda', PARAMS.get('gpu', 0))
     else:
         device = torch.device('cpu')
-    model_path = Path(os.getenv('OUTPUT_PATH'), f'{bert_model}-{method}_{os.getenv("MODEL_PATH")}')
+    model_path = Path(os.getenv('OUTPUT_PATH'), f'{bert_model}-{pretrained_model}-{method}_{os.getenv("MODEL_PATH")}')
     model.load_model(model_path)
     model.to(device)
 
@@ -36,12 +39,13 @@ def inference(bert_model, method='lstm'):
         all_preds = list()
         inference_dataloader = DataFrameDataLoader(
             df, pretrained_model=PARAMS[bert_model]['pretrained_model'],
+            do_lower_case=PARAMS[bert_model]['do_lower_case'],
             batch_size=PARAMS['evaluate']['batch_size'], max_len=PARAMS[bert_model]['eval_max_len']
         )
         for idx, (label, text, offsets) in enumerate(tqdm(inference_dataloader)):
             predicted_label = model(text, offsets)
             predicted_label = (predicted_label > 0.5).squeeze(dim=-1)
-            all_preds += [predicted_label.detach().cpu().numpy()]
+            all_preds += [predicted_label.detach().cpu().numpy().astype(int)]
     all_preds = np.concatenate(all_preds, axis=0)
     df[PARAMS['label']] = all_preds
     return df
--- scripts/train_bert.py
+++ scripts/train_bert.py
@@ -9,7 +9,6 @@ from pathlib import Path
 from data_loader.bert_dataloaders import DataFrameDataLoader
 from dotenv import load_dotenv
 
-
 load_dotenv('envs/.env')
 
 
@@ -17,10 +16,13 @@ with open('params.yaml', 'r') as f:
     PARAMS = yaml.safe_load(f)
 
 
-def start_training(bert_model, method='basic'):
+def start_training(bert_model, pretrained_model, method='basic'):
     try:
         model_module = importlib.import_module(f'model.{bert_model}.{method}')
-        model = model_module.Model(**PARAMS[bert_model], **PARAMS[bert_model][method])
+        model = model_module.Model(
+            **PARAMS[bert_model], **PARAMS[bert_model][method],
+            pretrained_model=pretrained_model
+        )
     except Exception as e:
         raise e
     if torch.cuda.is_available():
@@ -30,14 +32,15 @@ def start_training(bert_model, method='basic'):
     model.to(device)
 
     try:
-        trainer_module = importlib.import_module(f'training.{bert_model}.{method}')
-        trainer = trainer_module.Trainer(model, mode='train')
+        trainer_module = importlib.import_module(f'training.{bert_model}')
+        trainer = trainer_module.Trainer(model, pretrained_model=pretrained_model, mode='train')
     except Exception as e:
         raise e
 
     df = pd.read_csv('data/all.csv')
     dataloader = DataFrameDataLoader(
         df, pretrained_model=PARAMS[bert_model]['pretrained_model'],
+        do_lower_case=PARAMS[bert_model]['do_lower_case'],
         batch_size=PARAMS['train']['batch_size'],
         shuffle=PARAMS['validate']['shuffle'], max_len=PARAMS[bert_model]['max_len']
     )
@@ -51,10 +54,16 @@ def start_training(bert_model, method='basic'):
 
 
 if __name__ == '__main__':
-    bert_model, method = sys.argv[1], sys.argv[2]
-    results, losses_df = start_training(bert_model, method)
-    results_path = Path(os.getenv('OUTPUT_PATH'), f'{bert_model}-{method}_{os.getenv("RESULTS_PATH")}')
+    bert_model, pretrained_model, method = sys.argv[1], sys.argv[2], sys.argv[3]
+    results, losses_df = start_training(bert_model, pretrained_model, method)
+    results_path = Path(
+        os.getenv('OUTPUT_PATH'),
+        f'{bert_model}-{pretrained_model}-{method}_{os.getenv("RESULTS_PATH")}'
+    )
     with open(results_path, 'w') as f:
         json.dump(results, f)
-    plots_path = Path(os.getenv('OUTPUT_PATH'), f'{bert_model}-{method}_{os.getenv("PLOTS_PATH")}')
+    plots_path = Path(
+        os.getenv('OUTPUT_PATH'),
+        f'{bert_model}-{pretrained_model}-{method}_{os.getenv("PLOTS_PATH")}'
+    )
     losses_df.to_csv(plots_path, index=False)
