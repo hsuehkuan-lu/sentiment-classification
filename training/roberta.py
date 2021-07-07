4fa596c240fe4ecd653b27125ad7e7984a5da4f6
hsuehkuan-lu <s810142000@gmail.com>
2021-07-07 22:39:29 +0800

    replace warm up with constant

--- training/roberta.py
+++ training/roberta.py
@@ -1,7 +1,7 @@
 import yaml
 import torch
 from transformers import AdamW
-from transformers.optimization import get_linear_schedule_with_warmup
+from transformers.optimization import get_constant_schedule_with_warmup
 from training.base import TrainerBase
 
 with open('params.yaml', 'r') as f:
@@ -11,8 +11,7 @@ with open('params.yaml', 'r') as f:
 class Trainer(TrainerBase):
     def __init__(self, model, dataloader, method, mode):
         super(Trainer, self).__init__(model, dataloader, method, mode)
-        self._optimizer = AdamW(self._model.parameters(), lr=float(PARAMS[mode]['optimizer']['lr']))
-        self._scheduler = get_linear_schedule_with_warmup(
-            self._optimizer, num_warmup_steps=int(len(dataloader) * 0.1),
-            num_training_steps=len(dataloader) * int(PARAMS[mode]['epochs'])
+        self._optimizer = AdamW(self._model.parameters(), lr=float(PARAMS[mode]['optimizer']['lr']), correct_bias=False)
+        self._scheduler = get_constant_schedule_with_warmup(
+            self._optimizer, num_warmup_steps=int(len(dataloader) * 0.1)
         )
