93b636c6937661e28435e1862ce58945be0a806f
Peter Lu <Peter_HK_Lu@wistron.com>
2021-07-08 13:25:12 +0800

    update xlnet trainer

--- training/xlnet.py
+++ training/xlnet.py
@@ -1,7 +1,7 @@
 import yaml
 import torch
 from transformers import AdamW
-from transformers.optimization import get_constant_schedule_with_warmup
+from transformers.optimization import get_linear_schedule_with_warmup
 from training.base import TrainerBase
 
 with open('params.yaml', 'r') as f:
@@ -12,6 +12,7 @@ class Trainer(TrainerBase):
     def __init__(self, model, dataloader, method, mode):
         super(Trainer, self).__init__(model, dataloader, method, mode)
         self._optimizer = AdamW(self._model.parameters(), lr=float(PARAMS[mode]['optimizer']['lr']))
-        self._scheduler = get_constant_schedule_with_warmup(
-            self._optimizer, num_warmup_steps=int(len(dataloader) * 0.1)
+        self._scheduler = get_linear_schedule_with_warmup(
+            self._optimizer, num_warmup_steps=int(len(dataloader) * 0.1),
+            num_training_steps=len(dataloader) * int(PARAMS[mode]['epochs'])
         )
